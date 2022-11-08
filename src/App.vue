e42ec3c6e11c057b0e4c452a1dce231cb7479644
LOPA <danylospam@gmail.com>
2022-11-08 15:37:55 +0100

    Add old files

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Shadows Into Light Two");
+ $font-family: Shadows Into Light Two, sans-serif;
+.fontjoe{
+font-family: Shadows Into Light Two, sans-serif !important;;
+}
+</style>
+
+
+<template>
+  <v-app class="fontjoe"> 
+    <v-main>
+      <NavbarDan />
+      <Landing />
+      <AboutJ />
+      <Carousel />
+      <Pricing />
+      <Footerbar />
+    </v-main> 
+  </v-app>
+</template>
+
+<script>
+
+import Landing from './components/Core/Landing.vue'
+import Footerbar from './components/Core/Footerbar.vue'
+import AboutJ from './components/Core/AboutJ.vue'
+import Carousel from './components/Core/Carousel.vue'
+import Pricing from './components/Core/Pricing.vue'
+import NavbarDan from './components/Core/NavbarDan.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Landing,
+    Footerbar,
+    AboutJ,
+    Carousel,
+    Pricing,
+    NavbarDan
+},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
